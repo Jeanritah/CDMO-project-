56c35c2c78f3045a75d4120a9cccf81daf3827b2
esthy13 <giulianoesther@gmail.com>
2025-11-11 20:39:27 +0100

    function to save on json results from different solvers

---
+++ source/CP/run_cp.py
@@ -0,0 +1,5 @@
+## function to transform time from milliseconds to seconds
+	
+# By the definition of the floor function, the correct way to approach this problem is:
+# divide the time in milliseconds by 1000 (if your solver returns time in milliseconds)
+# apply the function floor to it
--- source/utils/utils.py
+++ source/utils/utils.py
@@ -3,28 +3,28 @@ import json
 import os
 
 
-def save_result(end_time:float, sol:str, file_path, obj=None, optimal=True, solver_name="gecode"):
+def save_result(tot_time:int, sol:str, file_path:str, obj=None, solver_name="gecode"):
     """
     Save the result to a JSON file under a solver key (e.g. 'gecode', 'chuffed').
     If the file exists, update or add the solver result.
 
     Args:
         solver_name (str): Name of the solver (used as key in the JSON file).
-        end_time (float): The time the computation ended.
+        tot_time (int): The time the computation took in seconds.
         sol (any): The solution to be saved.
         file_path (str): Path to the JSON file.
         obj (float, optional): Objective function value. Defaults to None without an objective function,
-        optimal (bool, optional): Whether the solution is optimal. Defaults to True.
     """
-    #TODO check whether time is optimal or not and eventually override default value
-    #TODO By the definition of the floor function, the correct way to approach this problem is:
-    # divide the time in milliseconds by 1000 (if your solver returns time in milliseconds)
-    # apply the function floor to it
 
     sol = ast.literal_eval(str(sol))
 
+    if tot_time < 300:
+        optimal = True
+    else:
+        optimal = False
+
     new_result = {
-        "time": end_time,
+        "time": tot_time,
         "optimal": optimal,
         "obj": obj,
         "sol": sol
@@ -45,7 +45,6 @@ def save_result(end_time:float, sol:str, file_path, obj=None, optimal=True, solv
     # Update or add new solver result
     data[solver_name] = new_result
 
-    #TODO check how the overwriting works (in case of multiple solvers)
     # Write back to file
     with open(file_path, "w") as outfile:
         json.dump(data, outfile, indent=4)
